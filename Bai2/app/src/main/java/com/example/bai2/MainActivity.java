9f187a28684acdc1777f5ed0faccb50b0e0f8839
longdzggg <vulong127z@gmail.com>
2025-08-12 09:39:00 +0700

    Bai 2

---
+++ Bai2/app/src/main/java/com/example/bai2/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.bai2;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onDestroy() {
+// TODO Auto-generated method stub
+        super.onDestroy();
+        Toast.makeText(this,"CR424 - on Destroy()",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+// TODO Auto-generated method stub
+        super.onPause();
+        Toast.makeText(this," CR424 - onPause", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onRestart() {
+// TODO Auto-generated method stub
+        super.onRestart();
+        Toast.makeText(this," CR424 - onRestart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onResume() {
+// TODO Auto-generated method stub
+
+        super.onResume();
+        Toast.makeText(this," CR424 - onResume", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStart() {
+// TODO Auto-generated method stub
+        super.onStart();
+        Toast.makeText(this," CR424 - onStart", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStop() {
+// TODO Auto-generated method stub
+        super.onStop();
+        Toast.makeText(this," CR424 - onStop", Toast.LENGTH_SHORT).show();
+    }
+    Button btncall;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                Intent intent1 = new Intent(MainActivity.this, subactivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
